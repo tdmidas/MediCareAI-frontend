73a0fb13a7b18d626af6005ab2c49718bca5ce46
tdmidas <tranduongminhdai@gmail.com>
2024-04-16 22:42:16 +0700

    add navigate home button

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -11,7 +11,9 @@ import { auth, googleAuthProvider, signInWithPopup, db, facebookAuthProvider } f
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { setDoc, doc, collection } from "firebase/firestore";
 import axios from "axios";
-import { Flex, Image } from "antd";
+import { Flex, Image, Button } from "antd";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+
 import loginBg from "../../assets/login-bg.png";
 const googleLogo = require('../../assets/google-18px.svg').default;
 const facebookLogo = require('../../assets/facebook-18px.svg').default;
@@ -96,66 +98,70 @@ const Login = () => {
 		signInWithEmail();
 	};
 	return (
-		<Flex wrap="wrap" align="center" justify="center" gap="large" style={{ maxHeight: "100vh", overflowY: 'scroll', padding: "20px" }}>
+		<>
+			<Flex style={{ padding: 20 }}>
+				<Button type="text" icon={<ArrowLeftOutlined />} onClick={() => navigate('/')} > Về trang chủ</Button>
+			</Flex>
+			<Flex wrap="wrap" align="center" justify="center" gap="large" style={{ maxHeight: "100vh", overflowY: 'scroll', padding: "20px" }}>
 
-			<div className={styles.container}>
-				<Flex direction="column" align="center" gap="small" wrap="wrap">
+				<div className={styles.container}>
+					<Flex direction="column" align="center" gap="small" wrap="wrap">
 
-					<form className={styles.formLogin} onSubmit={submitHandler} autoComplete="off">
-						<h2 > <b>Chăm sóc sức khỏe của bạn <br />tốt hơn với AI</b></h2>
-						<h3>Đăng nhập tài khoản để bắt đầu sử dụng</h3>				<div>
-							<div>
-								<input
-									type="text"
-									name="email"
-									value={data.email}
-									placeholder="E-mail"
-									onChange={changeHandler}
-									onFocus={focusHandler}
-									autoComplete="off"
-								/>
-								<img src="#" alt="" />
+						<form className={styles.formLogin} onSubmit={submitHandler} autoComplete="off">
+							<h2 > <b>Chăm sóc sức khỏe của bạn <br />tốt hơn với AI</b></h2>
+							<h3>Đăng nhập tài khoản để bắt đầu sử dụng</h3>				<div>
+								<div>
+									<input
+										type="text"
+										name="email"
+										value={data.email}
+										placeholder="E-mail"
+										onChange={changeHandler}
+										onFocus={focusHandler}
+										autoComplete="off"
+									/>
+									<img src="#" alt="" />
+								</div>
 							</div>
-						</div>
-						<div>
 							<div>
-								<input
-									type="password"
-									name="password"
-									value={data.password}
-									placeholder="Password"
-									onChange={changeHandler}
-									onFocus={focusHandler}
-									autoComplete="off"
-								/>
-								<img src="#" alt="" />
+								<div>
+									<input
+										type="password"
+										name="password"
+										value={data.password}
+										placeholder="Password"
+										onChange={changeHandler}
+										onFocus={focusHandler}
+										autoComplete="off"
+									/>
+									<img src="#" alt="" />
+								</div>
 							</div>
-						</div>
 
-						<div>
-							<button type="submit">Đăng nhập</button>
-							<button className={styles['facebook-btn']}
-								onClick={signInWithFacebook}>
-								<img src={facebookLogo} style={{ paddingRight: 10 }} /> Đăng nhập với Facebook
-							</button>
-							<button className={styles['google-btn']}
-								onClick={signInWithGoogle}>
-								<img src={googleLogo} style={{ paddingRight: 10 }} /> Đăng nhập với Google
-							</button>
-							<span style={{ color: "#a29494", textAlign: "center", display: "inline-block", width: "100%" }}>
-								Bạn chưa có tài khoản? <Link to="/register">Tạo tài khoản</Link>
-							</span>
-						</div>
+							<div>
+								<button type="submit">Đăng nhập</button>
+								<button className={styles['facebook-btn']}
+									onClick={signInWithFacebook}>
+									<img src={facebookLogo} style={{ paddingRight: 10 }} /> Đăng nhập với Facebook
+								</button>
+								<button className={styles['google-btn']}
+									onClick={signInWithGoogle}>
+									<img src={googleLogo} style={{ paddingRight: 10 }} /> Đăng nhập với Google
+								</button>
+								<span style={{ color: "#a29494", textAlign: "center", display: "inline-block", width: "100%" }}>
+									Bạn chưa có tài khoản? <Link to="/register">Tạo tài khoản</Link>
+								</span>
+							</div>
 
-					</form>
-					<ToastContainer />
+						</form>
+						<ToastContainer />
 
-					<Image preview={false} src={loginBg} alt="auth background" style={{ minWidth: 300, maxHeight: 650, borderRadius: 30, padding: 20 }} />
-				</Flex>
+						<Image preview={false} src={loginBg} alt="auth background" style={{ minWidth: 300, maxHeight: 650, borderRadius: 30, padding: 20 }} />
+					</Flex>
 
-			</div>
-		</Flex>
-
+				</div>
+			</Flex>
+		</>
 	);
 };
 
